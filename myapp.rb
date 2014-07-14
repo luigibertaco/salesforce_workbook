3d18421e96cc54723adfe6db60e1da1f704c5f32
Luigi Bertaco Cristofolini <luigibertaco@gmail.com>
2014-07-14 19:45:51 -0300

    oauth

--- myapp.rb
+++ myapp.rb
@@ -1,8 +1,41 @@
 require 'sinatra/base'
+require "omniauth"
+require "omniauth-salesforce"
 
 class MyApp < Sinatra::Base
+
+	configure do
+		enable :logging
+		enable :sessions
+		set :session_secret, ENV['SECRET']
+	end
+
+	use OmniAuth::Builder do
+		provider :salesforce, ENV['SALESFORCE_KEY'], ENV['SALESFORCE_SECRET']
+	end
+
+	get '/authenticate' do
+	    redirect "/auth/salesforce"
+	end
+
+	get '/auth/salesforce/callback' do
+		logger.info "#{env["omniauth.auth"]["extra"]["display_name"]}
+		authenticated"
+		env["omniauth.auth"]["extra"]["display_name"]
+	end
+
+	get '/auth/failure' do
+		params[:message]
+	end
+	
+	get '/unauthenticate' do
+		session.clear
+		'Goodbye - you are now logged out'
+	end
+
     get '/' do
-       'Home'
+    	logger.info "Visited home page"
+    	'Home 2'
 	end
     
     get '/hello' do
@@ -10,5 +43,5 @@ class MyApp < Sinatra::Base
 	end
     
     run! if app_file == $0
-    
+
 end
