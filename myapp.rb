184c436977e0653b3a6edf9774554f4fd442b909
Luigi Bertaco Cristofolini <luigibertaco@gmail.com>
2014-07-14 19:05:32 -0300

    sintra ok

---
+++ myapp.rb
@@ -0,0 +1,14 @@
+require 'sinatra/base'
+
+class MyApp < Sinatra::Base
+    get '/' do
+       'Home'
+	end
+    
+    get '/hello' do
+       'Hello World'
+	end
+    
+    run! if app_file == $0
+    
+end
